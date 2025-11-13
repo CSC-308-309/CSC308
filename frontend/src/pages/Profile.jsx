d6b55a31baf968ec318804bee192eb5e8281e7f6
Alisa <alisa.klyushina@gmail.com>
2025-11-12 17:28:38 -0800

    profile picture and banner added and editable

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,12 +1,28 @@
-// src/pages/Home.jsx
+// src/pages/Profile.jsx
 import Navbar from "../components/Navbar";
+import CoverPhoto from "../components/CoverPhoto";
+import EditProfilePhoto from "../components/ProfilePhoto";
+import TopProfileCard from "../components/TopProfileCard";
 
 export default function Profile() {
   return (
     <div className="flex h-screen overflow-hidden">
-          <div className="flex">
-            <Navbar />
-          </div>
+      <div className="flex">
+        <Navbar />
+      </div>
+      
+      <main className="flex-1 overflow-y-auto bg-gray-50">
+        <div className="mx-auto justify-center p-6">
+          <TopProfileCard>
+            <div className="relative">
+              <CoverPhoto />
+              <div className="absolute left-14 -bottom-[68px]">
+                <EditProfilePhoto />
+              </div>
+            </div>
+          </TopProfileCard>
+        </div>
+      </main>
     </div>
   );
 }
