2eff5039b59531d0a28e5871279b14e54a6ca03c
“abeyahc” <abeyahc@gmail.com>
2025-11-15 17:17:17 -0800

    update events page

---
+++ frontend/src/components/EventsTitle.jsx
@@ -0,0 +1,7 @@
+export default function EventsTitle() {
+    return (
+        <div className="max-w-[1200px] w-full h-[86px] bg-[#ECE6F0] mx-auto mt-6 rounded-lg px-[62px] items-center justify-start flex pl-8">
+            <h1 className="text-2xl font-bold text-[#73316C]">Events</h1>
+        </div>
+    );
+}
--- frontend/src/pages/Events.jsx
+++ frontend/src/pages/Events.jsx
@@ -1,10 +1,18 @@
 import Navbar from "../components/Navbar";
+import EventsTitle from "../components/EventsTitle";
+import EventsPage from "../components/EventsPage";
 
 export default function Events() {
     return (
         <div className="flex h-screen overflow-hidden">
             <div className="flex">
                 <Navbar />
+            </div>
+            <div className="flex-1 overflow-auto">
+                <EventsTitle />
+                <div className="mt-[10px]">
+                    <EventsPage />
+                </div>
             </div>
         </div>
     );
