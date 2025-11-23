6c6fb333917cf6c66196c5d2d3617ce1948b114e
“abeyahc” <abeyahc@gmail.com>
2025-11-22 17:02:33 -0800

    update concert memories [need to fix bugs]

--- frontend/src/components/TopProfileCard.jsx
+++ frontend/src/components/TopProfileCard.jsx
@@ -7,7 +7,7 @@ export default function TopProfileCard({ children }) {
         bg-white rounded-[20px]
         w-[1160px] h-[540px]
         flex-shrink-0 shadow-md
-        relative mx-auto pt-[0.5px]
+        relative pt-[0.5px]
       "
       style={{
         background: 'linear-gradient(0deg, #FFF 0%, #FFF 100%), #C4C4C4',
