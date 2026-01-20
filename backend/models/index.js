7ae9366dcd47a292f79f5414fafef2031391e636
Eliška Jelinek <elcaj.sp@gmail.com>
2026-01-19 18:02:46 -0800

    backend implementation of messaging api routes. setup mock data too

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,9 +1,11 @@
 import { ProfileModel } from './Profile.js';
 import { InteractionsModel } from './Interactions.js';
+import { MessagesModel } from './Messages.js';
 // add more models here as you create them
 
 export const dbModels = {
   Profile: ProfileModel,
   Interactions: InteractionsModel,
+  Messages: MessagesModel,
   // add them here as well
 };
