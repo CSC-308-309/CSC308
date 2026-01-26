1bbfd9a6e25b2a8080887bc2b2918b7af50fe145
Eliška Jelinek <elcaj.sp@gmail.com>
2026-01-26 10:45:44 -0800

    added messages model to index

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,11 +1,6 @@
-<<<<<<< HEAD
-import { ProfileModel } from "./Profile.js";
-import { InteractionsModel } from "./Interactions.js";
-=======
 import { ProfileModel } from './Profile.js';
 import { InteractionsModel } from './Interactions.js';
 import { MessagesModel } from './Messages.js';
->>>>>>> 7ae9366 (backend implementation of messaging api routes. setup mock data too)
 // add more models here as you create them
 
 export const dbModels = {
