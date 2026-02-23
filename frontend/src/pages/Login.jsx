5f0293ca4c51687f68db4582c6f46259e214313d
abeyahc <abeyahc@gmail.com>
2026-02-23 11:26:23 -0800

    Revert "Merge pull request #59 from CSC-308-309/Taliavivrett-CICD-bugs"
    This reverts commit 78a111039a340539cd3060a4643a7f4d970c3983, reversing
    changes made to 7779dab3302da13e9e0117f2bc9d7e6e32043640.

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -11,6 +11,7 @@ export default function Login() {
   const [error, setError] = useState("");
   const [isLoading, setIsLoading] = useState(false);
 
+  const handleClose = () => navigate(-1);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
